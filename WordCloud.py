54e483be3a4da5db3b8758c75e6bd4644108a587
Ducnguyen147 <102163508+Ducnguyen147@users.noreply.github.com>
2022-04-17 11:36:15 +0200

    Add files via upload

---
+++ WordCloud.py
@@ -0,0 +1,13 @@
+import matplotlib.pyplot as plt
+from wordcloud import WordCloud, ImageColorGenerator, STOPWORDS
+import numpy as np
+from PIL import Image
+
+text=open('C:\\Users\ADmin\Documents\wordcloud.txt','r').read()
+
+wc=WordCloud()
+wc.generate(text)
+
+plt.imshow(wc,interpolation='bilinear')
+plt.axis=('off')
+plt.show()
